7810a43004bf6b169f16a6a5e64310929d5759ad
brisklm <briskluo@outlook.com>
2025-08-23 05:37:32 +0800

    Automated code update 2025-08-22 21:37:31

--- code/config.py
+++ code/config.py
@@ -3,8 +3,9 @@ from datetime import datetime
 import numpy as np
 try:
     from nltk.sentiment.vader import SentimentIntensityAnalyzer
+    sia = SentimentIntensityAnalyzer()
 except Exception:
-    SentimentIntensityAnalyzer = None
+    sia = None
 try:
     import optuna
 except Exception:
@@ -30,17 +31,7 @@ CG_API_KEY = os.getenv('CG_API_KEY', 'CG-xA5NyokGEVbc4bwrvJPcpZvT')
 HELIUS_API_KEY = os.getenv('HELIUS_API_KEY', '70ed65ce-4750-4fd5-83bd-5aee9aa79ead')
 HELIUS_RPC_URL = os.getenv('HELIUS_RPC_URL', 'https://mainnet.helius-rpc.com')
 BITQUERY_API_KEY = os.getenv('BITQUERY_API_KEY', 'ory_at_LmFLzUutMY8EVb-P_PQVP9ntfwUVTV05LMal7xUqb2I.vxFLfMEoLGcu4XoVi47j-E2bspraTSrmYzCt1A4y2k')
-FEATURES = ['log_return_lag1', 'log_return_lag2', 'log_return_lag3', 'log_return_lag4', 'log_return_lag5', 'log_return_lag6', 'log_return_lag7', 'sign_return', 'sign_return_lag1', 'sign_return_lag2', 'sign_return_lag3', 'sign_return_lag4', 'sign_return_lag5', 'momentum_filter_1', 'momentum_filter_2', 'momentum_filter_3', 'momentum_4', 'momentum_5', 'volume_change_lag1', 'volatility_7d', 'sentiment_score']
-NAN_HANDLING = 'ffill'
+NAN_HANDLING = 'mean'
 LOW_VARIANCE_THRESHOLD = 0.01
-LGBM_PARAMS = {
-    'objective': 'regression',
-    'metric': 'rmse',
-    'max_depth': 6,
-    'num_leaves': 31,
-    'learning_rate': 0.05,
-    'feature_fraction': 0.9,
-    'reg_alpha': 0.1,
-    'reg_lambda': 0.1
-}
-OPTUNA_TRIALS = 50
+FEATURES = ['log_return_lag1', 'log_return_lag2', 'log_return_lag3', 'log_return_lag4', 'log_return_lag5', 'log_return_lag6', 'log_return_lag7', 'sign_return', 'sign_return_lag1', 'sign_return_lag2', 'sign_return_lag3', 'sign_return_lag4', 'sign_return_lag5', 'momentum_filter_1', 'momentum_filter_2', 'momentum_filter_3', 'momentum_4', 'vader_compound', 'vader_pos', 'vader_neg', 'vader_neu']
+BEST_PARAMS = {'max_depth': 5, 'num_leaves': 20, 'reg_alpha': 0.1, 'reg_lambda': 0.1, 'n_estimators': 200}
