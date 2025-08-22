ae37efd91929554d090cf66716d73ad198a6e7c6
brisklm <briskluo@outlook.com>
2025-08-22 22:59:40 +0800

    Automated code update 2025-08-22 14:59:37

--- code/config.py
+++ code/config.py
@@ -25,31 +25,26 @@ TRAINING_DAYS = int(os.getenv('TRAINING_DAYS', 365))
 MINIMUM_DAYS = 180
 REGION = os.getenv('REGION', 'com')
 DATA_PROVIDER = os.getenv('DATA_PROVIDER', 'binance')
-MODEL = os.getenv('MODEL', 'LSTM_Hybrid')
+MODEL = os.getenv('MODEL', 'LGBM')
 CG_API_KEY = os.getenv('CG_API_KEY', 'CG-xA5NyokGEVbc4bwrvJPcpZvT')
 HELIUS_API_KEY = os.getenv('HELIUS_API_KEY', '70ed65ce-4750-4fd5-83bd-5aee9aa79ead')
 HELIUS_RPC_URL = os.getenv('HELIUS_RPC_URL', 'https://mainnet.helius-rpc.com')
 BITQUERY_API_KEY = os.getenv('BITQUERY_API_KEY', 'ory_at_LmFLzUutMY8EVb-P_PQVP9ntfwUVTV05LMal7xUqb2I.vxFLfMEoLGcu4XoVi47j-E2bspraTSrmYzCt1A4y2k')
-FEATURES = ['log_return_lag1', 'log_return_lag2', 'log_return_lag3', 'log_return_lag4', 'log_return_lag5', 'sign_return', 'sign_return_lag1', 'sign_return_lag2', 'sign_return_lag3', 'momentum_filter_1', 'momentum_filter_2', 'momentum_filter_3', 'momentum_filter_4', 'vader_sentiment_compound']
-OPTUNA_TRIALS = 500
+FEATURES = ['log_return_lag1', 'log_return_lag2', 'log_return_lag3', 'log_return_lag4', 'log_return_lag5', 'sign_return', 'sign_return_lag1', 'sign_return_lag2', 'sign_return_lag3', 'momentum_filter_1', 'momentum_filter_2', 'momentum_filter_3', 'momentum_filter_4', 'vader_sentiment_compound', 'log_return_lag6', 'log_return_lag7', 'ema_12', 'ema_26', 'rsi_14']
+OPTUNA_TRIALS = 1000
 MODEL_PARAMS = {
-    'LSTM_Hybrid': {
-        'units': 64,
-        'dropout': 0.3,
-        'epochs': 200,
-        'batch_size': 64
-    },
-    'LightGBM': {
+    'LGBM': {
         'objective': 'regression',
         'metric': 'rmse',
-        'num_leaves': 20,
+        'verbose': -1,
+        'max_depth': 6,
+        'num_leaves': 31,
         'learning_rate': 0.01,
-        'n_estimators': 200,
-        'max_depth': 5,
-        'reg_alpha': 0.2,
-        'reg_lambda': 0.2
+        'n_estimators': 1000,
+        'reg_alpha': 0.1,
+        'reg_lambda': 0.1
     }
 }
 NAN_HANDLING = 'ffill'
-ENSEMBLE_MODELS = ['LSTM_Hybrid', 'LightGBM']
-VARIANCE_THRESHOLD = 0.01
+LOW_VARIANCE_THRESHOLD = 0.01
+SMOOTHING_WINDOW = 3
